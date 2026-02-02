54f1bd4b99cd9b6c8370fd35772ebdd32653c03f
TrungOdin <trung100982@gmail.com>
2026-02-02 20:13:52 +0700

    style: Increase modal backdrop opacity to black/80

--- components/ui/Modal.tsx
+++ components/ui/Modal.tsx
@@ -25,7 +25,7 @@ export default function Modal({ isOpen, onClose, children, title, width = 'max-w
 
                 {/* Background overlay - Darker as requested */}
                 <div
-                    className="fixed inset-0 bg-gray-900 bg-opacity-75 transition-opacity"
+                    className="fixed inset-0 bg-black/80 transition-opacity"
                     aria-hidden="true"
                     onClick={onClose}
                 ></div>
